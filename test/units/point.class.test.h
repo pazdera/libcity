4de1dc59e62d3474ecd7e40b7ec3ee2eaaec01df
Radek Pazdera <radek.pazdera@gmail.com>
2011-02-28 13:33:37 +0100

    Unittests added

---
+++ test/units/point.class.test.h
@@ -0,0 +1,42 @@
+/**
+ * This code is part of libcity library.
+ *
+ * @file point.class.test.h
+ * @date 23.02.2011
+ * @author Radek Pazdera (xpazde00@stud.fit.vutbr.cz)
+ *
+ * @brief Unit test of Point class
+ *
+ * Unit test is a class derived from Unit. It's defined
+ * inline in the declaration.
+ */
+
+// Includes
+#include <string>
+#include <iostream>
+
+#include <stdlib.h>
+#include <assert.h>
+
+// Modules
+#include "../unit.h"
+#include "../../src/geometry/point.h"
+
+class PointClassTest : public Unit
+{
+  public:
+    PointClassTest()
+      : Unit()
+    {
+      
+    }
+/*
+  private:
+    bool */
+
+  public:
+    bool run()
+    {
+      return true;
+    }
+};
